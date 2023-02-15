3d5a77634330a8a903ab899a0153c3a4b45ca3da
Sophia <99284300+sophiav17@users.noreply.github.com>
2023-02-15 12:56:00 +0000

    Add files via upload

---
+++ table.js
@@ -0,0 +1,31 @@
+img = "";
+status = "";
+
+function preload()
+{
+    img = loadImage('table.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
